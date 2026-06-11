9b78cb9c2bd69881371e6ff3c9d8308f1e00a407
Aditya Singh <adityasingh24915@gmail.com>
2026-06-11 15:31:36 +0530

    Add files via upload

---
+++ Day-4/armstrongnumber in range.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main() {
+    int start, end, num, temp, remainder;
+    int result;
+
+    printf("Enter the starting number: ");
+    scanf("%d", &start);
+
+    printf("Enter the ending number: ");
+    scanf("%d", &end);
+
+    printf("Armstrong numbers between %d and %d are:\n", start, end);
+
+    for (num = start; num <= end; num++) {
+        temp = num;
+        result = 0;
+
+        while (temp != 0) {
+            remainder = temp % 10;
+            result += remainder * remainder * remainder;
+            temp /= 10;
+        }
+
+        if (result == num)
+            printf("%d ", num);
+    }
+
+    return 0;
+}
---
+++ Day-4/fibonacci series.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+    int n, i,first = 0, second = 1, next;
+
+    printf("Enter the number of terms: ");
+    scanf("%d", &n);
+
+    printf("Fibonacci Series:\n");
+
+    for (i = 0; i < n; i++) {
+        printf("%d ", first);
+        next = first + second;
+        first = second;
+        second = next;
+    }
+
+    return 0;
+}
